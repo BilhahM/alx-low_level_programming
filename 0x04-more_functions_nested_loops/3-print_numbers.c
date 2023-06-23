9e93fc6cf49f5ad1844ba1a8687239b5630a77f3
BilhahM <you@example.com>
2023-06-23 07:55:36 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print i
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar (i);
+	}
+	_putchar ('\n');
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  more_numbers - print more numbers
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar((i / 10) + 48);
+			}
+		_putchar((i % 10) + 48);
+		}
+	_putchar('\n');
+	}
+}
